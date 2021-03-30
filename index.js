92b6e9da3da9deda111ee97e49e48a26261b5213
Jacob Marthaller <marthaller.jr@gmail.com>
2021-03-30 08:19:04 -0700

    working solution

---
+++ index.js
@@ -0,0 +1,25 @@
+function arrayToList(arr) {
+    let list = null;
+    for (let i = arr.length - 1; i >= 0; i--) {
+      list = {value: arr[i], rest: list};
+    }
+    return list;
+  }
+  
+  function listToArray(list) {
+    let arr = [];
+    for (let node = list; node; node = node.rest) {
+      arr.push(node.value);
+    }
+    return arr;
+  }
+  
+  function prepend(value, list) {
+    return {value, rest: list};
+  }
+  
+  function nth(list, n) {
+    if (!list) return undefined;
+    else if (n == 0) return list.value;
+    else return nth(list.rest, n - 1);
+}
